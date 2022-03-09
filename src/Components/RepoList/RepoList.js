5abf05fe09d9f81b14d3528fb2a1519b81b55318
CYBER-MARCUSSEN <83762741+CYBER-MARCUSSEN@users.noreply.github.com>
2022-03-09 15:35:06 +0100

    2

--- src/Components/RepoList/RepoList.js
+++ src/Components/RepoList/RepoList.js
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 
 import { userSelector } from "../../redux/user-reducer/selectors";
 import "./RepoList.css";
 
 const RepoList = () => {
-    const { userRepos, searchUserRepo, filteredRepos } =
-    useSelector(userSelector);
+    
     const getRepos = useSelector((state)=> {return state.user?.filteredRepos?.items});
 
     return (
